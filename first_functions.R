fe4d9707b67a33c74108a624034f3fdbd59c63cd
Igor Geyn <igorgeyn@gmail.com>
2021-05-11 11:16:31 -0700

    adding first script

---
+++ first_functions.R
@@ -0,0 +1,26 @@
+### This is just to generate some super basic functions to test in the R package framework.
+### You should install and load (at least install) the `dplyr` package if you have not yet done so.
+
+## base R
+adding_two <- function(x) {
+  y = x + 2
+  print(y)
+}
+
+story_mod <- function(num_bears) {
+  paste('Goldilocks would have been better with',num_bears,'bears instead of three.')
+}
+
+## dplyr
+## note this saves to global
+dplyr_manip <- function(df, var_half, var_double) {
+  require('dplyr')
+  var_half <- sym(var_half)
+  var_double <- sym(var_double)
+  df_out <<- 
+    df %>% 
+    mutate(new_var1 = !!var_half/2,
+           new_var2 = !!var_double*2) %>% 
+    rename(var_half_new = new_var1,
+           var_double_new = new_var2)
+}
