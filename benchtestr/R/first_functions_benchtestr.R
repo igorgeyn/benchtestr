fe1b04403205511974b78abd5f2ff3010e8b33ce
Shing-hon Lam <43804359+shinghon@users.noreply.github.com>
2021-05-14 17:52:53 +0800

    Update first_functions_benchtestr.R

--- benchtestr/R/first_functions_benchtestr.R
+++ benchtestr/R/first_functions_benchtestr.R
@@ -24,3 +24,33 @@ dplyr_manip <- function(df, var_half, var_double) {
     rename(var_half_new = new_var1,
            var_double_new = new_var2)
 }
+
+library(sensemakr)
+library(boot)
+library(randomizr)
+
+## Nonparametric estimation of robustness value
+## Slow, suggest putting R = 10 to test
+benchmakr_rvq = function(sensitivity_stats = "rv_q",
+                         data = darfur,
+                         formula = "peacefactor ~ directlyharmed + age + farmer_dar + herder_dar +
+                         pastvoted + hhsize_darfur + female + village", 
+                         slice_prop = .5, 
+                         treatment = "directlyharmed", 
+                         bm_cov = "female", 
+                         kd = 1:3,
+                         R = 250){
+  slice = simple_ra(nrow(data), prob = slice_prop)
+  boot_fun = function(data, slice){
+    data = rbind(data, data[slice,])
+    model = lm(formula = as.formula(formula), data = data)
+    out = sensemakr(model, treatment = treatment, benchmark_covariates = bm_cov, kd = kd)
+    #out = out[["sensitivity_stats"]][[sensitivity_stats]] %>% as.numeric()   # Not working, set "rv_q" by default  
+    out = out[["sensitivity_stats"]][["rv_q"]] %>% as.numeric()
+    print(out)
+  }
+  out = boot(data = data, statistic = boot_fun, R = R, parallel = "multicore")
+  print(out)
+}
+
+benchmakr_rvq_test = benchmakr_rvq(data = darfur, R = 10)
