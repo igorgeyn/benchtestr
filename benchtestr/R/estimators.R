98a947a092c22f8e6cf4dfabe0acab9f513effeb
Shing-hon Lam <43804359+shinghon@users.noreply.github.com>
2021-05-26 20:15:22 +0800

    Update estimators.R

--- benchtestr/R/estimators.R
+++ benchtestr/R/estimators.R
@@ -49,3 +49,59 @@ naive_dim_comparison <- function(df1, df2, treatment, outcome) {
 
 }
 
+## Bug fixed to improve generalisability 
+naive_dim_comparison <- function(df1, df2, treatment, outcome) {
+  require(tidyverse)
+  options(dplyr.summarise.inform = F)
+  # initialize a df for final output
+  # final_output <<- as.data.frame(matrix())
+  treatment_sym <- sym(treatment)
+  outcome_sym <- sym(outcome)
+  df1 <- df1 %>% mutate_at(treatment, as.numeric)
+  df2 <- df2 %>% mutate_at(treatment, as.numeric)
+  dim_estimator_df1 <- df1 %>%
+    group_by(!!treatment_sym) %>%
+    summarise(avg_earnings = mean(!!outcome_sym),
+              earnings_var = var(!!outcome_sym),
+              count = n())
+  
+  # need to input treatment_length fxn here
+  # need to input control_length fxn here
+  
+  dim_estimate1 <- filter(dim_estimator_df1, !!treatment_sym == 1)$avg_earnings - filter(dim_estimator_df1, !!treatment_sym == 0)$avg_earnings
+  # see above for how to fix se
+  se_dim1 <- dim_estimator_df1 %>% mutate(se_cal = earnings_var / count) %>% colSums()
+  se_dim1 <- se_dim1["se_cal"] %>% sqrt() %>% as.numeric()
+  
+  # create the df of treatment-only from df1
+  df1_treated_only <- df1 %>%
+    filter(!!treatment_sym == 1 | !!treatment_sym == T) %>%
+    dplyr::select(matches(paste(treatment, outcome, sep = "|")))
+
+  # create df2 and bind treatment-only from df1 to it
+  dim_estimator_df2 <- df2 %>%
+    drop_na(!!treatment_sym) %>% drop_na(!!outcome_sym) %>%
+    dplyr::select(matches(paste(treatment, outcome, sep = "|"))) %>%
+    rbind(df1_treated_only) %>%
+    group_by(!!treatment_sym) %>%
+    summarise(avg_earnings = mean(!!outcome_sym, na.rm = TRUE),
+              earnings_var = var(!!outcome_sym, na.rm = TRUE),
+              count = n())
+  
+  # need to input treatment_length fxn here
+  # need to input control_length fxn here
+  
+  dim_estimate2 <- filter(dim_estimator_df2, !!treatment_sym == 1)$avg_earnings - filter(dim_estimator_df2, !!treatment_sym == 0)$avg_earnings
+  se_dim2 <- dim_estimator_df2 %>% mutate(se_cal = earnings_var / count) %>% colSums()
+  se_dim2 <- se_dim2["se_cal"] %>% sqrt() %>% as.numeric()
+  
+  final_output <- c(dim_estimate1, se_dim1, dim_estimate2, se_dim2)
+  names(final_output) <- c("dim_estimate1", "se_dim1", "dim_estimate2", "se_dim2")
+  
+  return(final_output)
+}
+
+data("lalonde")
+naive_dim_comparison(lalonde, lalonde, "treat", "re78")
+naive_dim_comparison(causaldata::Mroz, causaldata::Mroz, "lfp", "inc")
+
